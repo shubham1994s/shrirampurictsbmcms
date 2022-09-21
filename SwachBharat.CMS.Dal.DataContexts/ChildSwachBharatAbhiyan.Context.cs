e13296516bc357b346db9cd128303cf9cbae36f7
milind wankhede <milindawankhede@gmail.com>
2022-09-21 12:02:48 +0530

    comit by milind

--- SwachBharat.CMS.Dal.DataContexts/ChildSwachBharatAbhiyan.Context.cs
+++ SwachBharat.CMS.Dal.DataContexts/ChildSwachBharatAbhiyan.Context.cs
@@ -58,6 +58,7 @@ namespace SwachBharat.CMS.Dal.DataContexts
         public virtual DbSet<EmpBeatMap> EmpBeatMaps { get; set; }
         public virtual DbSet<Vehical_QR_Master> Vehical_QR_Master { get; set; }
         public virtual DbSet<GarbageCollectionDetail> GarbageCollectionDetails { get; set; }
+        public virtual DbSet<SauchalayAddress> SauchalayAddresses { get; set; }
         public virtual DbSet<EmpShift> EmpShifts { get; set; }
         public virtual DbSet<UserMaster> UserMasters { get; set; }
         public virtual DbSet<DumpYardDetail> DumpYardDetails { get; set; }
@@ -67,7 +68,6 @@ namespace SwachBharat.CMS.Dal.DataContexts
         public virtual DbSet<VehicleRegistration> VehicleRegistrations { get; set; }
         public virtual DbSet<HouseList> HouseLists { get; set; }
         public virtual DbSet<Daily_Attendance> Daily_Attendance { get; set; }
-        public virtual DbSet<SauchalayAddress> SauchalayAddresses { get; set; }
     
         public virtual ObjectResult<GetAttendenceDetailsTotal_Result> GetAttendenceDetailsTotal(Nullable<int> userId, Nullable<int> year, Nullable<int> month)
         {
